b4dd650a1436f58eafab6bc6cfb35405e1ec9990
Ju Li <ju.li@stern.nyu.edu>
2025-03-06 20:29:02 +0000

    updated factory

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,54 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Order, OrderItems
 
 
-class YourResourceModelFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+class OrderFactory(factory.Factory):
+    """Creates fake orders"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Order
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    customer_id = factory.Sequence(lambda n: n)
+    order_status = factory.Faker(
+        "random_element", elements=("Received", "Shipped", "Delivered")
+    )
+    order_created = factory.Faker("date_time")
+    order_updated = factory.Faker("date_time")
 
-    # Todo: Add your other attributes here...
+    @factory.post_generation
+    def order_items(
+        self, create, extracted, **kwargs
+    ):  # pylint: disable=method-hidden, unused-argument
+        """Creates the order items list"""
+        if not create:
+            return
+
+        if extracted:
+            self.order_items = extracted
+
+
+class OrderItemsFactory(factory.Factory):
+    """Creates fake order items"""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Maps factory to data model"""
+
+        model = OrderItems
+
+    id = factory.Sequence(lambda n: n)
+    product_id = factory.Sequence(lambda n: n)
+    quantity = factory.Sequence(lambda n: n)
+    price = factory.Faker(
+        "random_number",
+        digits=2,
+        fix_len=False,
+        min=1.00,
+        max=20.00,
+    )
+    order = factory.SubFactory(OrderFactory)
+    order_id = factory.SelfAttribute("order.id")
